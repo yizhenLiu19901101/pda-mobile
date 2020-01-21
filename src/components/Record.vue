664a3cea7604559bb9ca359dbf1ff83ddd2bad9c
milo19901101 <liuyizhen19901101@163.com>
2020-01-21 17:24:53 +0800

    时间筛选

--- src/components/Record.vue
+++ src/components/Record.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="page-navbar">
-    <van-tabs class = "page-part" v-model = "currentTag" @click = "change">
+    <van-tabs color = "#1989fa" class = "page-part" v-model = "currentTag" @click = "change">
       <van-tab v-for = "(tag,index) in tagList" v-bind:key = "index"
       :id = "tag" :title = 'tag.menuName'>
       </van-tab>
@@ -12,8 +12,9 @@
       <my-time-line :items = 'items'/>
     </span>
     <span v-if = "currentTag == 1">
+      <van-divider><span v-on:click.prevent = "changeTimeType">{{ timeType }}</span></van-divider>
       <div style = "margin-top: 5%">
-        <van-tabs v-model = "consumeType" type = "card" style = "width: 60%;margin: 0 auto" @click = "changeConsumeType">
+        <van-tabs v-model = "consumeType" color = "#1989fa" type = "card" style = "width: 60%;margin: 0 auto" @click = "changeConsumeType">
           <van-tab title = "总支出" name = "2" />
           <van-tab title = "总收入" name = "3" />
           <van-tab title = "净收入" name = "4" />
@@ -21,6 +22,27 @@
        </div>
       <ring :data = "chartData" :legend-visible = "false" :settings = "chartSettings" :graphic = "graphic" />
     </span>
+    <!--时间类型选择组件 -->
+    <van-popup v-model = "showTimeTypeComponent" position = "bottom">
+      <van-radio-group v-model = "timeType">
+        <van-cell-group>
+          <van-cell title = "当日" clickable @click = "handleTimeTypeRadio(1,'当日')">
+            <van-radio slot = "right-icon" name = "当日" />
+          </van-cell>
+          <van-cell title = "本周" clickable @click = "handleTimeTypeRadio(2,'本周')">
+            <van-radio slot = "right-icon" name = "本周" />
+          </van-cell>
+          <van-cell title = "当前月" clickable @click = "handleTimeTypeRadio(3,'当前月')">
+            <van-radio slot = "right-icon" name = "当前月" />
+          </van-cell>
+          <van-cell title = "自定义" clickable @click = "handleTimeTypeRadio(4,'自定义')">
+            <van-radio slot = "right-icon" name = "自定义" />
+          </van-cell>
+        </van-cell-group>
+      </van-radio-group>
+    </van-popup>
+    <!-- 日历组件 -->
+    <van-calendar v-model = "showCalendarComponent" type="range" @confirm = "onConfirm" />
   </div>
 </template>
 <script>
@@ -37,11 +59,17 @@ export default {
   },
   data () {
     return {
+      showTimeTypeComponent: false,
+      showCalendarComponent: false,
       consumeType: 1,
       menuId: this.$store.state.currentMenu,
       tagList: this.$store.state.tagList,
       currentTag: this.$store.state.tagList[0].menuId,
       items: this.$store.state.items,
+      timeType: '当日',
+      timeTypeValue: 1,
+      startDate: null,
+      endDate: null,
       title: this.$store.state.items.sum == null ? null : this.$store.state.items.sum,
       chartData: {
         columns: ['item', 'money'],
@@ -75,19 +103,51 @@ export default {
     }
   },
   methods: {
+    onConfirm (date) {
+      console.log('date =' + JSON.stringify(date))
+      const [start, end] = date
+      this.startDate = start
+      this.endDate = end
+      console.log(this.startDate + '' + this.endDate)
+      this.showCalendarComponent = false
+      // 查询数据
+      this.queryData(this.consumeType, this.startDate, this.endDate, this.timeTypeValue)
+    },
+    handleTimeTypeRadio (typeIndex, type) {
+      this.timeType = type
+      this.timeTypeValue = typeIndex
+      this.showTimeTypeComponent = false
+      if (typeIndex === 4) {
+        this.showCalendarComponent = true
+      } else {
+        // 查询数据
+        this.queryData(this.consumeType, null, null, this.timeTypeValue)
+      }
+    },
+    changeTimeType () {
+      this.showTimeTypeComponent = true
+    },
+    // 切换页签
     change (index) {
       let val = this.tagList[index]
       // 查询目标数据
-      this.queryData(val.menuId - 7)
+      this.queryData(val.menuId - 7, null, null, null)
     },
-    // 修改收支类型
+    // 切换收支类型
     changeConsumeType (index) {
       // 查询目标数据
-      this.queryData(index)
+      this.queryData(index, null, null, null)
     },
     // 查询财务数据
-    queryData (queryType) {
-      axios.get('/finance/getDetailDate/' + queryType, {
+    queryData (queryType, startDate, endDate, timeTypeValue) {
+      let financeDetailDto = {
+        timeTypeValue: timeTypeValue,
+        queryType: queryType,
+        endDate: endDate,
+        startDate: startDate
+      }
+      axios.post('/finance/getDetailDate', {
+        data: financeDetailDto,
         headers: {
           'token': this.$store.state.token
         }
